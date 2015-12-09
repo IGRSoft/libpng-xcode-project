11e0355987551650ba3191e53d9d28bf8123b19f
Vitalii Parovishnyk <Korich.vi.p@gmail.com>
2015-12-09 11:49:36 +0200

    Updated lib for v1.6.20 Updated Project settings Setuped test for all targets

--- xcode/libpng-test/libpng_test.m
+++ xcode/libpng-test/libpng_test.m
@@ -100,11 +100,4 @@
     XCTAssert(result == 0, @"Pass");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
 @end
